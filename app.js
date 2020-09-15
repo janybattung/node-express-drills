40c68b07af3fdbfa9a75e9fb3e641f0976fc0e94
Jany Battung <janybattung@gmail.com>
2020-09-15 10:08:47 -0700

    update files

--- app.js
+++ app.js
@@ -7,6 +7,98 @@ const app = express();
 // on their way to the final handler
 app.use(morgan('dev')); //dev - predefined format
 
+
+app.get('/grade', (req, res) => {
+  // get the mark from the query
+  const { mark } = req.query;
+
+  // do some validation
+  if (!mark) {
+    // mark is required
+    return res
+      .status(400)
+      .send('Please provide a mark');
+  }
+
+  const numericMark = parseFloat(mark);
+  if (Number.isNaN(numericMark)) {
+    // mark must be a number
+    return res
+      .status(400)
+      .send('Mark must be a numeric value');
+  }
+
+  if (numericMark < 0 || numericMark > 100) {
+    // mark must be in range 0 to 100
+    return res
+      .status(400)
+      .send('Mark must be in range 0 to 100');
+  }
+
+  if (numericMark >= 90) {
+    return res.send('A');
+  }
+
+  if (numericMark > 80) {
+    return res.send('B');
+  }
+
+  if (numericMark >= 70) {
+    return res.send('C');
+  }
+
+  res.send('F');
+});
+
+
+
+
+
+
+
+
+
+
+
+app.get('/colors', (req, res) => {
+  const colors = [
+    {
+      name: "red",
+      rgb: "FF0000"
+    },
+    {
+      name: "green",
+      rgb: "00FF00"
+    },
+    {
+      name: "blue",
+      rgb: "0000FF"
+    },
+  ];
+  res.json(colors);
+});
+
+
+
+app.get('/video', (req, res) => {
+  const video = {
+    title: 'Cats falling over',
+    description: '15 minutes of hilarious fun as cats fall over',
+    length: '15.40'
+  }
+  res.json(video);
+});
+
+
+
+
+app.get('/hello', (req, res) => {
+    res
+      .status(400)
+      .send('Oops! You did it again.');
+  });
+
+
 //DRILL #1
 app.get('/sum', (req, res) => {
     const {a, b} = req.query;
